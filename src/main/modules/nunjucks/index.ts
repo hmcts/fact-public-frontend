1da3f0f1301547f37503058b112de9fa6fe5f19e
hurricanepilot <jonathan.coomber@justice.gov.uk>
2026-01-16 15:16:33 +0000

    Alternative jstag lookup based on deployment env

--- src/main/modules/nunjucks/index.ts
+++ src/main/modules/nunjucks/index.ts
@@ -1,18 +1,26 @@
 import * as path from 'path';
 
+import { Logger } from '@hmcts/nodejs-logging';
 import * as express from 'express';
 import * as nunjucks from 'nunjucks';
 
 export interface DynatraceOptions {
-  env?: string;
+  jstagKey: string;
   jstags: {
-    prod: string;
-    nonProd: string;
+    [index: string]: string;
   };
 }
 
+const logger = Logger.getLogger('nunjucks');
+
 export class Nunjucks {
-  constructor(private readonly dynatrace: DynatraceOptions, public readonly developmentMode: boolean) {}
+  private readonly jstag: string;
+  constructor(dynatrace: DynatraceOptions, public readonly developmentMode: boolean) {
+    this.jstag = dynatrace?.jstags[dynatrace.jstagKey] ?? null;
+    if (this.jstag === null) {
+      throw new Error('Dynatrace jstag not found');
+    }
+  }
 
   enableFor(app: express.Express): void {
     app.set('view engine', 'njk');
@@ -25,11 +33,8 @@ export class Nunjucks {
       express: app,
     });
     env.addGlobal('govukRebrand', true);
-
-    env.addGlobal(
-      'jstag',
-      'prod' === this.dynatrace.env?.toLowerCase() ? this.dynatrace.jstags.prod : this.dynatrace.jstags.nonProd
-    );
+    logger.info(`using jstag: ${this.jstag}`);
+    env.addGlobal('jstag', this.jstag);
 
     app.use((req, res, next) => {
       res.locals.pagePath = req.path;
