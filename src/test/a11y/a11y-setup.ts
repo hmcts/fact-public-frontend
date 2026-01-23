3640dd360df98845f076405b50c92d9747723667
Dan-Marius Bradea <bradeadanmarius@gmail.com>
2026-01-23 15:52:52 +0000

    incremental improvement removed consol log and redundant jenkins steps

--- src/test/a11y/a11y-setup.ts
+++ src/test/a11y/a11y-setup.ts
@@ -1,11 +1,7 @@
 import { startTestServer } from './utils/test-server';
 
 async function globalSetup(): Promise<void> {
-  const baseURL = await startTestServer();
-  // eslint-disable-next-line no-console
-  console.log(`Test server started at ${baseURL}`);
-
-  process.env.A11Y_TEST_URL = baseURL;
+  process.env.A11Y_TEST_URL = await startTestServer();
 }
 
 export default globalSetup;
--- src/test/a11y/a11y-teardown.ts
+++ src/test/a11y/a11y-teardown.ts
@@ -2,8 +2,6 @@ import { stopTestServer } from './utils/test-server';
 
 async function globalTeardown(): Promise<void> {
   await stopTestServer();
-  // eslint-disable-next-line no-console
-  console.log('Test server stopped');
 }
 
 export default globalTeardown;
