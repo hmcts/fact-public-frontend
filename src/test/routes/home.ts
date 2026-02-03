e589da2b8bb692f2487cd1158f9883232e5980dc
josh-uk <joshua.blackmoor@justice.gov.uk>
2026-02-03 15:04:34 +0000

    Undo tests

--- src/test/routes/home.ts
+++ src/test/routes/home.ts
@@ -3,7 +3,7 @@ import request from 'supertest';
 
 import { app } from '../../main/app';
 
-// TODO: replace this sample test with proper route tests for your application test
+// TODO: replace this sample test with proper route tests for your application
 
 describe('Home page', () => {
   describe('on GET', () => {
