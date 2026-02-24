3f73776ca70845010715e79513b31d8b0a08a1b3
Rebecca <67150587+RebeccaHayleyPickles@users.noreply.github.com>
2026-02-24 11:33:45 +0000

    Fact 2661 accessibility statement (#72)
    * Add accessibility statement page
    
    * Add tests
    
    * Fix linting issues
    
    * update brace expansion resolution
    
    * Add functional tests for accessibility page
    
    * Add accessibility checks for ''/accessibility-statement'
    
    * fixed typo
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: hurricanepilot <hurricanepilot@gmail.com>

---
+++ src/test/functional/tests/accessibility.spec.ts
@@ -0,0 +1,58 @@
+import { test } from '@playwright/test';
+
+import { AccessibilityPage } from '../page-objects/AccessibilityPage';
+import { HomePage } from '../page-objects/HomePage';
+
+test.describe('Accessibility Page Visual & Language Checks', () => {
+  test('should load and display correct content sections (english)', async ({ page }) => {
+    const accessibilityPage = new AccessibilityPage(page);
+    await accessibilityPage.goto('en');
+    await accessibilityPage.expectVisibleElements();
+    // ensure the language selection has the Welsh/Cymraeg toggle
+    await accessibilityPage.expectLanguageLinkToContainText('Cymraeg');
+  });
+
+  test('should load and display correct content sections (welsh)', async ({ page }) => {
+    const accessibilityPage = new AccessibilityPage(page);
+    await accessibilityPage.goto('cy');
+    await accessibilityPage.expectVisibleElements();
+    // ensure the language selection has the English toggle
+    await accessibilityPage.expectLanguageLinkToContainText('English');
+  });
+
+  test('should maintain preselected language during navigation', async ({ page }) => {
+    const homePage = new HomePage(page);
+    const accessibilityPage = new AccessibilityPage(page);
+    await homePage.goto('en');
+    await accessibilityPage.goto();
+    await accessibilityPage.expectVisibleElements();
+    // ensure the language selection has the Cymraeg toggle
+    await accessibilityPage.expectLanguageLinkToContainText('Cymraeg');
+
+    await homePage.goto('cy');
+    await accessibilityPage.goto();
+    await accessibilityPage.expectVisibleElements();
+    // ensure the language selection has the English toggle
+    await accessibilityPage.expectLanguageLinkToContainText('English');
+  });
+});
+
+test.describe('Accessibility Page Content Checks', () => {
+  test('should have content and show the correct page heading (english)', async ({ page }) => {
+    const accessibilityPage = new AccessibilityPage(page);
+    await accessibilityPage.goto('en');
+    await accessibilityPage.expectMainContentToBePopulated();
+    await accessibilityPage.expectHeadingToContainText(
+      'Accessibility statement for the ‘Find a Court or Tribunal’ service'
+    );
+  });
+
+  test('should have content and show the correct page heading (welsh)', async ({ page }) => {
+    const accessibilityPage = new AccessibilityPage(page);
+    await accessibilityPage.goto('cy');
+    await accessibilityPage.expectMainContentToBePopulated();
+    await accessibilityPage.expectHeadingToContainText(
+      'Datganiad hygyrchedd ar gyfer y gwasanaeth ‘Dod o hyd i lys neu dribiwnlys’'
+    );
+  });
+});
