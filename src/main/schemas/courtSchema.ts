b58844624815bb5a131243f8c0352fbb50bb269e
josh-uk <joshua.blackmoor@justice.gov.uk>
2026-02-26 15:13:52 +0000

    Edited courtSchema

--- src/main/schemas/courtSchema.ts
+++ src/main/schemas/courtSchema.ts
@@ -71,10 +71,10 @@ const courtContactDescriptionSchema = z.object({
 
 const courtContactDetailSchema = z.object({
   courtContactDescriptionId: z.string(),
-  explanation: z.string(),
-  explanationCy: z.string(),
-  email: z.string(),
-  phoneNumber: z.string(),
+  explanation: z.string().nullable(),
+  explanationCy: z.string().nullable(),
+  email: z.string().nullable(),
+  phoneNumber: z.string().nullable(),
   courtContactDescription: courtContactDescriptionSchema,
 });
 
@@ -130,7 +130,7 @@ const hearingEnhancementEquipmentSchema = z.enum([
 
 const courtAccessibilityOptionSchema = z.object({
   accessibleParking: z.boolean(),
-  accessibleParkingPhoneNumber: z.string(),
+  accessibleParkingPhoneNumber: z.string().nullable(),
   accessibleToiletDescription: z.string().nullable(),
   accessibleToiletDescriptionCy: z.string().nullable(),
   accessibleEntrance: z.boolean(),
