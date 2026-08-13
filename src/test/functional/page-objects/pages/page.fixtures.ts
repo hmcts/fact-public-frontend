964ac442493b462ee2ecc4ed63f71a30577a1f11
hurricanepilot <hurricanepilot@gmail.com>
2026-08-13 15:52:14 +0100

    migrate "not found" page so that the a11y and perf tests work

--- src/test/functional/page-objects/pages/page.fixtures.ts
+++ src/test/functional/page-objects/pages/page.fixtures.ts
@@ -2,11 +2,13 @@ import { Page } from '@playwright/test';
 
 import { AccessibilityPage } from './accessibility.po';
 import { HomePage } from './home.po';
+import { NotFoundPage } from './not-found.po';
 
 export interface PageFixtures {
   determinePage: Page;
   accessibilityPage: AccessibilityPage;
   homePage: HomePage;
+  notFoundPage: NotFoundPage;
 }
 
 export const pageFixtures = {
@@ -21,5 +23,9 @@ export const pageFixtures = {
   // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   homePage: async ({ determinePage }, use): Promise<void> => {
     await use(new HomePage(determinePage));
+  },
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  notFoundPage: async ({ determinePage }, use): Promise<void> => {
+    await use(new NotFoundPage(determinePage));
   },
 };
--- src/test/functional/tests/pages/not-found.spec.ts
+++ src/test/functional/tests/pages/not-found.spec.ts
@@ -1,28 +1,21 @@
-import { test } from '@playwright/test';
+import { test } from '../../fixtures';
 
-import { HomePage } from '../page-objects/HomePage';
-import { NotFoundPage } from '../page-objects/NotFoundPage';
-
-test.describe('Not Found Page Visual & Language Checks', () => {
-  test('should load and display correct content sections (english)', async ({ page }) => {
-    const notFoundPage = new NotFoundPage(page);
+test.describe('Not Found Page Visual & Language Checks', { tag: '@functional' }, () => {
+  test('should load and display correct content sections (english)', async ({ notFoundPage }) => {
     await notFoundPage.goto('en');
     await notFoundPage.expectVisibleElements();
     // ensure the language selection has the Welsh/Cymraeg toggle
     await notFoundPage.expectLanguageLinkToContainText('Cymraeg');
   });
 
-  test('should load and display correct content sections (welsh)', async ({ page }) => {
-    const notFoundPage = new NotFoundPage(page);
+  test('should load and display correct content sections (welsh)', async ({ notFoundPage }) => {
     await notFoundPage.goto('cy');
     await notFoundPage.expectVisibleElements();
     // ensure the language selection has the English toggle
     await notFoundPage.expectLanguageLinkToContainText('English');
   });
 
-  test('should maintain preselected language during navigation', async ({ page }) => {
-    const homePage = new HomePage(page);
-    const notFoundPage = new NotFoundPage(page);
+  test('should maintain preselected language during navigation', async ({ notFoundPage, homePage }) => {
     await homePage.goto('en');
     await notFoundPage.goto();
     await notFoundPage.expectVisibleElements();
@@ -37,16 +30,14 @@ test.describe('Not Found Page Visual & Language Checks', () => {
   });
 });
 
-test.describe('Not Found Page Content Checks', () => {
-  test('should have content and show the correct page heading (english)', async ({ page }) => {
-    const notFoundPage = new NotFoundPage(page);
+test.describe('Not Found Page Content Checks', { tag: '@functional' }, () => {
+  test('should have content and show the correct page heading (english)', async ({ notFoundPage }) => {
     await notFoundPage.goto('en');
     await notFoundPage.expectMainContentToBePopulated();
     await notFoundPage.expectHeadingToContainText('Find a Court or Tribunal');
   });
 
-  test('should have content and show the correct page heading (welsh)', async ({ page }) => {
-    const notFoundPage = new NotFoundPage(page);
+  test('should have content and show the correct page heading (welsh)', async ({ notFoundPage }) => {
     await notFoundPage.goto('cy');
     await notFoundPage.expectMainContentToBePopulated();
     await notFoundPage.expectHeadingToContainText('Dod o hyd i lys neu dribiwnlys');
