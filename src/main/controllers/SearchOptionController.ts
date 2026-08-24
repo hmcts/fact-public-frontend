412e0115d93d490fa607e31621f87a0b2434d32f
hurricanepilot <hurricanepilot@gmail.com>
2026-08-24 12:03:15 +0100

    Use BaseController as parent for controllers that only render views

--- src/main/controllers/SearchOptionController.ts
+++ src/main/controllers/SearchOptionController.ts
@@ -3,21 +3,21 @@ import { Response } from 'express';
 
 import { FactRequest } from '../interfaces/FactRequest';
 
+import BaseController from './BaseController';
+
 @route('/search-option')
-export default class SearchOptionController {
+export default class SearchOptionController extends BaseController {
   @GET()
   public get(req: FactRequest, res: Response): void {
-    const data = req.i18n.getDataByLanguage(req.lng).search.option;
-    res.render('search/option', data);
+    this.renderView(req, res, 'search/option', 'search.option');
   }
 
   @POST()
   public post(req: FactRequest, res: Response): void {
-    const data = req.i18n.getDataByLanguage(req.lng).search.option;
     const knowsLocation = typeof req.body?.knowsLocation === 'string' ? req.body.knowsLocation : undefined;
 
     if (!knowsLocation) {
-      return res.render('search/option', { ...data, errors: true });
+      return this.renderView(req, res, 'search/option', 'search.option', { errors: true });
     }
 
     switch (knowsLocation) {
@@ -27,7 +27,7 @@ export default class SearchOptionController {
         return res.redirect('/service-choose-action');
       default:
         // Defensive handling for crafted/invalid POST values outside of radio options
-        return res.render('search/option', { ...data, errors: true });
+        return this.renderView(req, res, 'search/option', 'search.option', { errors: true });
     }
   }
 }
