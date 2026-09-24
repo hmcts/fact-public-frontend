e96b05171a98e8b29d973779234ca4e24a2a8552
ibrahimelsanosi-moj <ibrahim.elsanosi@justice.gov.uk>
2026-09-24 17:39:20 +0100

    FACT-3056 add unit test

--- src/test/unit/views/components/useful-information.test.ts
+++ src/test/unit/views/components/useful-information.test.ts
@@ -1,6 +1,7 @@
 import { env } from '../helpers/nunjucksEnv';
 
-const i18n = require('../../../../main/locales/en/court.json');
+const courtI18n = require('../../../../main/locales/en/court.json');
+const serviceCentreI18n = require('../../../../main/locales/en/service-centre.json');
 
 describe('Useful information macro', () => {
   test('renders useful information links with opens-in-new-tab text', () => {
@@ -10,12 +11,12 @@ describe('Useful information macro', () => {
     `;
 
     const html = env.renderString(template, {
-      usefulInformationText: i18n.usefulInformation,
+      usefulInformationText: courtI18n.usefulInformation,
     });
 
-    expect(html).toContain(i18n.usefulInformation.mainHeading);
-    expect(html).toContain(i18n.usefulInformation.comingToCourtUrlText);
-    expect(html).toContain(i18n.usefulInformation.opensInNewTab);
+    expect(html).toContain(courtI18n.usefulInformation.mainHeading);
+    expect(html).toContain(courtI18n.usefulInformation.comingToCourtUrlText);
+    expect(html).toContain(courtI18n.usefulInformation.opensInNewTab);
   });
 
   test('can render only the scammers subsection', () => {
@@ -25,12 +26,41 @@ describe('Useful information macro', () => {
     `;
 
     const html = env.renderString(template, {
-      usefulInformationText: i18n.usefulInformation,
+      usefulInformationText: courtI18n.usefulInformation,
     });
 
-    expect(html).toContain(i18n.usefulInformation.mainHeading);
-    expect(html).toContain(i18n.usefulInformation.scammers);
-    expect(html).not.toContain(i18n.usefulInformation.comingToCourt);
-    expect(html).not.toContain(i18n.usefulInformation.hearingsAtThisCourt);
+    expect(html).toContain(courtI18n.usefulInformation.mainHeading);
+    expect(html).toContain(courtI18n.usefulInformation.scammers);
+    expect(html).not.toContain(courtI18n.usefulInformation.comingToCourt);
+    expect(html).not.toContain(courtI18n.usefulInformation.hearingsAtThisCourt);
+  });
+
+  test('renders Bury additional message for Bury slug', () => {
+    const template = `
+    {% from "components/useful-information.njk" import usefulInformation %}
+    {{ usefulInformation(usefulInformationText, false, serviceCentreSlug) }}
+  `;
+
+    const html = env.renderString(template, {
+      usefulInformationText: serviceCentreI18n.usefulInformation,
+      serviceCentreSlug: 'bury-st-edmunds-regional-divorce-centre',
+    });
+
+    expect(html).toContain(serviceCentreI18n.usefulInformation.scammers);
+    expect(html).toContain(serviceCentreI18n.usefulInformation.buryStEdmundsAdditionalMessage.split('\n')[0]);
+  });
+
+  test('does not render Bury additional message for non-Bury slug', () => {
+    const template = `
+    {% from "components/useful-information.njk" import usefulInformation %}
+    {{ usefulInformation(usefulInformationText, false, serviceCentreSlug) }}
+  `;
+
+    const html = env.renderString(template, {
+      usefulInformationText: serviceCentreI18n.usefulInformation,
+      serviceCentreSlug: 'other-centre',
+    });
+
+    expect(html).not.toContain(serviceCentreI18n.usefulInformation.buryStEdmundsAdditionalMessage.split('\n')[0]);
   });
 });
